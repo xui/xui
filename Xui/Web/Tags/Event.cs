8d2c782bd6a9e5a4317b734301343b81c6064c48
Rylan Barnes <rylan@barn.es>
2024-08-13 05:55:07 -0500

    Switch event to records

--- Xui/Web/Tags/Event.cs
+++ Xui/Web/Tags/Event.cs
@@ -1,83 +1,82 @@
 namespace Xui.Web.Html;
 
-public class Event
-{
+#pragma warning disable IDE1006 // Naming Styles
+
+public record class Event(
+    bool? altKey = null,
+    int? button = null,
+    int? buttons = null,
+    TouchPoint[]? changedTouches = null,
+    double? clientX = null,
+    double? clientY = null,
+    string? code = null,
+    bool? ctrlKey = null,
+    HtmlElement? currentTarget = null,
+    string? data = null,
+    DataTransfer? dataTransfer = null,
+    long? deltaMode = null,
+    double? deltaX = null,
+    double? deltaY = null,
+    double? deltaZ = null,
+    long? detail = null,
+    string? inputType = null,
+    bool? isComposing = null,
+    bool? isTrusted = null,
+    string? key = null,
+    double? layerX = null,
+    double? layerY = null,
+    string? locale = null,
+    int? location = null,
+    bool? metaKey = null,
+    double? movementX = null,
+    double? movementY = null,
+    double? offsetX = null,
+    double? offsetY = null,
+    double? pageX = null,
+    double? pageY = null,
+    HtmlElement? relatedTarget = null,
+    bool? repeat = null,
+    double? screenX = null,
+    double? screenY = null,
+    bool? shiftKey = null,
+    HtmlElement? target = null,
+    TouchPoint[]? targetTouches = null,
+    TouchPoint[]? touches = null,
+    string? type = null,
+    double? x = null,
+    double? y = null
+) {
     public static readonly Event Empty = new();
-
-    public bool? altKey { get; set; }
-    public int? button { get; set; }
-    public int? buttons { get; set; }
-    public TouchPoint[]? changedTouches { get; set; }
-    public double? clientX { get; set; }
-    public double? clientY { get; set; }
-    public string? code { get; set; }
-    public bool? ctrlKey { get; set; }
-    public HtmlElement? currentTarget { get; set; }
-    public string? data { get; set; }
-    public DataTransfer? dataTransfer { get; set; }
-    public long? deltaMode { get; set; }
-    public double? deltaX { get; set; }
-    public double? deltaY { get; set; }
-    public double? deltaZ { get; set; }
-    public long? detail { get; set; }
-    public string? inputType { get; set; }
-    public bool? isComposing { get; set; }
-    public bool? isTrusted { get; set; }
-    public string? key { get; set; }
-    public double? layerX { get; set; }
-    public double? layerY { get; set; }
-    public string? locale { get; set; }
-    public int? location { get; set; }
-    public bool? metaKey { get; set; }
-    public double? movementX { get; set; }
-    public double? movementY { get; set; }
-    public double? offsetX { get; set; }
-    public double? offsetY { get; set; }
-    public double? pageX { get; set; }
-    public double? pageY { get; set; }
-    public HtmlElement? relatedTarget { get; set; }
-    public bool? repeat { get; set; }
-    public double? screenX { get; set; }
-    public double? screenY { get; set; }
-    public bool? shiftKey { get; set; }
-    public HtmlElement? target { get; set; }
-    public TouchPoint[]? targetTouches { get; set; }
-    public TouchPoint[]? touches { get; set; }
-    public string? type { get; set; }
-    public double? x { get; set; }
-    public double? y { get; set; }
 }
 
-public class HtmlElement
-{
-    public string? id { get; set; }
-    public string? name { get; set; }
-    public string? type { get; set; }
-    public string? value { get; set; }
-}
+public record class HtmlElement(
+    string? id = null,
+    string? name = null,
+    string? type = null,
+    string? value = null
+);
 
-public class DataTransfer
-{
-    public string? dropEffect { get; set; }
-    public string? effectAllowed { get; set; }
-    public string[]? files { get; set; }
-    public DataTransferItem[]? items { get; set; }
-    public string[]? types { get; set; }
-}
+public record class DataTransfer(
+    string? dropEffect = null,
+    string? effectAllowed = null,
+    string[]? files = null,
+    DataTransferItem[]? items = null,
+    string[]? types = null
+);
 
-public class DataTransferItem
-{
-    public string? kind { get; set; }
-    public string? type { get; set; }
-}
+public record class DataTransferItem(
+    string? kind = null,
+    string? type = null
+);
 
-public class TouchPoint
-{
-    public long? identifier { get; set; }
-    public double? screenX { get; set; }
-    public double? screenY { get; set; }
-    public double? clientX { get; set; }
-    public double? clientY { get; set; }
-    public double? pageX { get; set; }
-    public double? pageY { get; set; }
-}
+public record class TouchPoint(
+    long? identifier = null,
+    double? screenX = null,
+    double? screenY = null,
+    double? clientX = null,
+    double? clientY = null,
+    double? pageX = null,
+    double? pageY = null
+);
+
+#pragma warning restore IDE1006 // Naming Styles
