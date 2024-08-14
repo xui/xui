b20ea5804f87f50cffbc311236d738dc1cd6e543
Rylan Barnes <rylan@barn.es>
2024-08-14 06:14:58 -0500

    Simplify for now

--- Demos/Website/UI/UI.cs
+++ Demos/Website/UI/UI.cs
@@ -11,21 +11,18 @@ partial class UI : UI<ViewModel>
         MapPage("/zero-javascript", ZeroJavaScript);
     }
 
-    [HttpXPage("/")]
     void Index(Context context)
     {
         context.ViewModel.Name = "Twas clicked";
         context.ViewModel.ShowAdditional = false;
     }
 
-    [HttpXPage("/zero-pages")]
     void ZeroPages(Context context)
     {
         context.ViewModel.Name = "Twas clicked";
         context.ViewModel.ShowAdditional = true;
     }
 
-    [HttpXPage("/zero-javascript")]
     void ZeroJavaScript(Context context)
     {
         context.ViewModel.Name = "OK, back to normal: Rylan Barnes";
