a9699ca196008494953fc69e693922dbf6c84c21
Rylan Barnes <rylan@barn.es>
2024-10-10 04:26:38 -0500

    Help me transition off that prototype please

--- Xui/Web/HttpX/HttpXContext.cs
+++ Xui/Web/HttpX/HttpXContext.cs
@@ -9,170 +9,60 @@ using Xui.Web;
 
 namespace Xui.Web.HttpX;
 
-public abstract partial class UI<T> where T : IViewModel
+public struct HttpXContext(WebSocketPipe? pipe)
 {
-    public class HttpXContext
+    public WebSocketPipe? Pipe { get; set; } = pipe;
+    public readonly bool IsWebSocketOpen => Pipe is not null && Pipe.State == WebSocketState.Open;
+
+    public static HttpXContext Get(HttpContext httpContext)
     {
-        private readonly UI<T> ui;
-        public T ViewModel { get; init; }
-        private WebSocketPipe? pipe;
-        private Composer composer = new();
-        private Composer composerCompare = new();
-        public bool IsWebSocketOpen => pipe?.State == WebSocketState.Open;
+        // TODO: Move to header approach?
+        var key = httpContext.Connection.Id;
+        return new HttpXContext(WebSocketPipe.Get(key));
+    }
 
-        private static readonly MemoryCache cache = new(new MemoryCacheOptions());
-        private static readonly MemoryCacheEntryOptions entryOptions =
-            new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromDays(1));
-
-        static int tmpCountSessions = 0; // TODO: Remove this after benchmarking is under control.
-        public static HttpXContext Get(HttpContext httpContext, UI<T> ui)
+    public async readonly Task UpdatePath(PathString path)
+    {
+        if (Pipe is not null && Pipe.State == WebSocketState.Open)
         {
-            // var sessionId = httpContext.Connection.Id;  // TODO: Remove this after benchmarking is under control.
-            var sessionId = httpContext.GetHttpXSessionId();
-            if (cache.Get(sessionId) is not HttpXContext context)
-            {
-                Console.WriteLine($"Context: {tmpCountSessions++} {httpContext.Connection.Id}");
-                context = new HttpXContext(ui);
-                Set(sessionId, context);
-            }
-            return context;
-        }
-
-        private static void Set(string id, HttpXContext context)
-        {
-            cache.Set(id, context, entryOptions);
-        }
-
-        public HttpXContext(UI<T> ui)
-        {
-            this.ui = ui;
-
-            // TODO: Need to be more clever about how a new ViewModel is created.
-            ViewModel = (T)T.New();
-        }
-
-        public void Compose()
-        {
-            using (composer.ReuseBuffer())
-            {
-                var html = ui.MainLayout(ViewModel);
-            }
-        }
-
-        internal async Task WriteResponseAsync(HttpContext httpContext)
-        {
-            Compose();
-
-            var contentLength = composer.GetContentLengthIfConvenient();
-            if (contentLength.HasValue)
-                httpContext.Response.ContentLength = contentLength.Value;
-
-            var pipeWriter = httpContext.Response.BodyWriter;
-            pipeWriter.Write(composer.AsSpan());
-            await pipeWriter.FlushAsync();
-        }
-
-        internal void Recompose()
-        {
-            using (composerCompare.ReuseBuffer())
-            {
-                var html = ui.MainLayout(ViewModel);
-            }
-        }
-
-        internal async Task Recompose(WebSocketPipe pipe)
-        {
-            Recompose();
-
-            if (!IsWebSocketOpen)
-                return;
-
-            var deltas = composer.GetDeltas(composerCompare);
-            if (deltas is null)
-                return;
-
-            var writer = pipe.Output;
-            writer.Write(deltas);
+            var writer = Pipe.Output;
+            writer.WriteStringLiteral("window.history.pushState({},'', '");
+            Encoding.UTF8.GetBytes(path.ToUriComponent().AsSpan(), writer);
+            writer.WriteStringLiteral("')");
             await writer.FlushAsync();
-
-            // Swap buffers.
-            (composer, composerCompare) = (composerCompare, composer);
         }
+    }
 
-        internal async Task PushHistoryState(string path)
+    public async Task AwaitEventListeners(HtmlDelegate html, CancellationToken cancellationToken)
+    {
+        if (Pipe is null)
+            return;
+
+        await Task.WhenAll(
+            Receive(html), 
+            Pipe.RunAsync(cancellationToken)
+        );
+    }
+
+    private async Task Receive(HtmlDelegate html)
+    {
+        if (Pipe is null)
+            return;
+
+        while (await Pipe.Input.ReadAsync() is var result && !result.IsCompleted)
         {
-            if (IsWebSocketOpen && pipe is not null)
-            {
-                var writer = pipe.Output;
-                writer.WriteStringLiteral($"window.history.pushState({{}},'', '{path}')");
-                Encoding.UTF8.GetBytes(path.AsSpan(), writer);
-                writer.WriteStringLiteral("')");
-                await writer.FlushAsync();
-            }
+            // TODO: Buffer might be multiple segments one day.
+            var buffer = result.Buffer.First;
+            var (slotId, domEvent) = Event.ParseEvent(buffer.Span);
+            Pipe.Input.AdvanceTo(result.Buffer.End);
+
+            var eventHandler = GetEventHandlerById(slotId, html);
+            EventLoop.Enqueue(eventHandler);
         }
+    }
 
-        internal async Task AssignWebSocket(WebSocketManager webSocketManager, HttpContext httpContext)
-        {
-            using (var webSocket = await webSocketManager.AcceptWebSocketAsync())
-            {
-                using (pipe = new WebSocketPipe(webSocket))
-                {
-                    // HotReload is a no-op in RELEASE mode.
-                    using (HotReload.Listen(async () => await Recompose(this.pipe)))
-                    {
-                        // TODO: This is almost correct.  
-                        // Works across multiple browsers but multiple tabs gets its Action stolen.
-                        // Rework this once you figure out the various ViewModel state levels.
-                        ViewModel.OnChanged = async () => await Recompose(pipe);
-
-                        await Task.WhenAll(
-                            Receive(pipe), 
-                            pipe.RunAsync(httpContext.RequestAborted)
-                        );
-                    }
-                }
-            }
-
-            pipe = null;
-            // TODO: Should ViewModel.OnChange unsubscribe here?
-        }
-
-        private async Task Receive(IDuplexPipe pipe)
-        {
-            while (await pipe.Input.ReadAsync() is var result && !result.IsCompleted)
-            {
-                // TODO: Buffer might be multiple segments one day.
-                var buffer = result.Buffer.First;
-                var (slotId, domEvent) = ParseEvent(buffer.Span);
-                using (this.ViewModel.Batch())
-                {
-                    composer.HandleEvent(slotId, domEvent);
-                }
-
-                pipe.Input.AdvanceTo(result.Buffer.End);
-            }
-        }
-
-        private (int, Event?) ParseEvent(ReadOnlySpan<byte> buffer)
-        {
-            int i = 0, slot = 0;
-            while (true)
-            {
-                // Convert from ASCII to int, digit by digit.
-                int d = buffer[i] - 48;
-                if (d >= 0 && d <= 9) {
-                    slot = slot * 10 + d;
-                    ++i;
-                    continue;
-                }
-
-                if (i >= buffer.Length - 1)
-                    return (slot, null);
-                
-                var message = buffer[i..];
-                var @event = JsonSerializer.Deserialize<Event>(message);
-                return (slot, @event);
-            }
-        }
+    private static Func<Event, Task> GetEventHandlerById(int slotId, HtmlDelegate html)
+    {
+        return null;
     }
 }
