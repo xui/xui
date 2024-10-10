bea523b1c7d7ea98e04c981b97b9af1faf1d062e
Rylan Barnes <rylan@barn.es>
2024-10-10 05:15:46 -0500

    Forgot the most important bools

--- Xui/Web/Html/Html.cs
+++ Xui/Web/Html/Html.cs
@@ -34,21 +34,21 @@ public readonly ref struct Html
         composer.Grow(literalLength, formattedCount);
     }
 
-    public readonly void AppendLiteral(string value) => composer.AppendLiteral(value);
-    public readonly void AppendFormatted(string value) => composer.AppendFormatted(value);
-    public readonly void AppendFormatted(int value, string? format = null) => composer.AppendFormatted(value, format);
-    public readonly void AppendFormatted(long value, string? format = null) => composer.AppendFormatted(value, format);
-    public readonly void AppendFormatted(float value, string? format = null) => composer.AppendFormatted(value, format);
-    public readonly void AppendFormatted(double value, string? format = null) => composer.AppendFormatted(value, format);
-    public readonly void AppendFormatted(decimal value, string? format = null) => composer.AppendFormatted(value, format);
-    public readonly void AppendFormatted(bool value) => composer.AppendFormatted(value);
-    public readonly void AppendFormatted(DateTime value, string? format = null) => composer.AppendFormatted(value, format);
-    public readonly void AppendFormatted(TimeSpan value, string? format = null) => composer.AppendFormatted(value, format);
-    public readonly void AppendFormatted<TView>(TView v) where TView : IView => AppendFormatted(v.Render());
-    public readonly void AppendFormatted(Html html) => composer.AppendFormatted(html);
-    public readonly void AppendFormatted(Slot slot) => AppendFormatted(slot());
-    public readonly void AppendFormatted(Action action) => composer.AppendFormatted(action);
-    public readonly void AppendFormatted(Action<Event> action) => composer.AppendFormatted(action);
-    public readonly void AppendFormatted(Func<Task> actionAsync) => composer.AppendFormatted(actionAsync);
-    public readonly void AppendFormatted(Func<Event, Task> actionAsync) => composer.AppendFormatted(actionAsync);
+    public readonly bool AppendLiteral(string value) => composer.AppendLiteral(value);
+    public readonly bool AppendFormatted(string value) => composer.AppendFormatted(value);
+    public readonly bool AppendFormatted(int value, string? format = null) => composer.AppendFormatted(value, format);
+    public readonly bool AppendFormatted(long value, string? format = null) => composer.AppendFormatted(value, format);
+    public readonly bool AppendFormatted(float value, string? format = null) => composer.AppendFormatted(value, format);
+    public readonly bool AppendFormatted(double value, string? format = null) => composer.AppendFormatted(value, format);
+    public readonly bool AppendFormatted(decimal value, string? format = null) => composer.AppendFormatted(value, format);
+    public readonly bool AppendFormatted(bool value) => composer.AppendFormatted(value);
+    public readonly bool AppendFormatted(DateTime value, string? format = null) => composer.AppendFormatted(value, format);
+    public readonly bool AppendFormatted(TimeSpan value, string? format = null) => composer.AppendFormatted(value, format);
+    public readonly bool AppendFormatted<TView>(TView v) where TView : IView => AppendFormatted(v.Render());
+    public readonly bool AppendFormatted(Html html) => composer.AppendFormatted(html);
+    public readonly bool AppendFormatted(Slot slot) => AppendFormatted(slot());
+    public readonly bool AppendFormatted(Action action) => composer.AppendFormatted(action);
+    public readonly bool AppendFormatted(Action<Event> action) => composer.AppendFormatted(action);
+    public readonly bool AppendFormatted(Func<Task> actionAsync) => composer.AppendFormatted(actionAsync);
+    public readonly bool AppendFormatted(Func<Event, Task> actionAsync) => composer.AppendFormatted(actionAsync);
 }
