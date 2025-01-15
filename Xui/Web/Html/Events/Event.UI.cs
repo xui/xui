698e66b44e69a0621274a96b71ad07fb761f77e9
Rylan Barnes <rylan@barn.es>
2025-01-14 18:45:31 -0600

    It makes no sense to have these below as their own interface

--- Xui/Web/Html/Events/Event.UI.cs
+++ Xui/Web/Html/Events/Event.UI.cs
@@ -17,9 +17,31 @@ public partial record class Event
     {
         // UIEvent
         long detail { get; }
+
+        // Event (base) note: it makes no sense to have these below as their own interface.
+        bool bubbles { get; }
+        bool cancelable { get; }
+        bool composed { get; }
+        HtmlElement currentTarget { get; }
+        bool defaultPrevented { get; }
+        int eventPhase { get; }
+        bool isTrusted { get; }
+        HtmlElement target { get; }
+        double timeStamp { get; }
+        string type { get; }
     }
 
     long UI.detail => this.detail ?? 0;
+    bool UI.bubbles => this.bubbles;
+    bool UI.cancelable => this.cancelable;
+    bool UI.composed => this.composed;
+    HtmlElement UI.currentTarget => this.currentTarget ?? new();
+    bool UI.defaultPrevented => this.defaultPrevented;
+    int UI.eventPhase => this.eventPhase;
+    bool UI.isTrusted => this.isTrusted;
+    HtmlElement UI.target => this.target ?? new();
+    double UI.timeStamp => this.timeStamp;
+    string UI.type => this.type;
 }
 
 #pragma warning restore IDE1006
