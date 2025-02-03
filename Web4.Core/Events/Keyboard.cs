c84b401b96292f85653b18d888e8cf113adf34d1
Rylan Barnes <rylan@barn.es>
2025-02-03 06:29:29 -0600

    KeyboardEvent can probably make use of a subset for keys

--- Web4.Core/Events/Keyboard.cs
+++ Web4.Core/Events/Keyboard.cs
@@ -4,32 +4,13 @@ namespace Web4;
 
 public partial interface Events
 {
-    public interface Keyboard: UI, Subsets.Modifiers, Subsets.IsComposing
+    public interface Keyboard: UI, Subsets.Modifiers, Subsets.IsComposing, Subsets.Keys
     {
         new const string Format = "code,key,location,repeat," + 
             UI.Format + "," + 
             Subsets.IsComposing.Format + "," +
-            Subsets.Modifiers.Format;
-        
-        /// <summary>
-        /// Returns a string with the code value of the physical key represented by the event.
-        /// </summary>
-        string Code { get; }
-
-        /// <summary>
-        /// Returns a string representing the key value of the key represented by the event.
-        /// </summary>
-        string Key { get; }
-
-        /// <summary>
-        /// Returns a number representing the location of the key on the keyboard or other input device.
-        /// </summary>
-        KeyLocation Location { get; }
-
-        /// <summary>
-        /// Returns a boolean value that is true if the key is being held down such that it is automatically repeating.
-        /// </summary>
-        bool Repeat { get; }
+            Subsets.Modifiers.Format + "," + 
+            Subsets.Keys.Format;
     }
 }
 
