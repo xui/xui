794d022f38444d4985400039846a8f5f60f4da00
Rylan Barnes <rylan@barn.es>
2025-03-28 05:51:55 -0500

    Knowing thread IDs is quite useful

--- Web4/Debug.cs
+++ Web4/Debug.cs
@@ -199,7 +199,7 @@ public static class Debug
         {
             var elapsed = Stopwatch.GetElapsedTime(sw1);
             long gc2 = GC.GetAllocatedBytesForCurrentThread();
-            Console.WriteLine($"{$"🚥 Perf({name}):",-35} elapsed:{$"{elapsed.TotalNanoseconds:n0} ns",-15} allocations: {(gc2 - gc1):n0} bytes");
+            Console.WriteLine($"{$"🚥 Perf({name}):",-35} elapsed:{$"{elapsed.TotalNanoseconds:n0} ns",-15} {$"allocations: {(gc2 - gc1):n0} bytes",-25}   thread:{Thread.CurrentThread.ManagedThreadId}");
         }
     }
 }
