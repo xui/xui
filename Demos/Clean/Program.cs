3bf62f0b1b1a663c93f97abfd57ce31233b99d64
Rylan Barnes <rylan@barn.es>
2025-06-18 08:45:26 -0500

    Checkpoint.  Why not.  It's not "clean" anymore anyway.

--- Demos/Clean/Program.cs
+++ Demos/Clean/Program.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Web4;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,8 @@ var app = builder.Build();
 
 string name = "World";
 int c = 0;
+double d = 3.14;
+string[] names = ["one", "two", "three", "four", "five", "six", "seven"];
 
 var window = app.MapWeb4("/app", () => $"""
     <html>
@@ -12,33 +15,90 @@ var window = app.MapWeb4("/app", () => $"""
             <button onclick={() => c++}>
                 Clicks: {c}
             </button>
+            {c:c} and {d:c}
 
-            <h1>Hello {name}!</h1>
-            {MyButton(text: "I'm a custom button!")}
-            {MyButton(text: "Nice to repeat you.", onClick: e => c--)}
+            {$"one {"two"} {3} four"}
+            {MyButton(text: "Nice to repeat you")}
+            {MyButton(text: "Nice to repeat you"):Fade}
+
+            <br />
+            <br />
+            {MyButton(text: "111")}{MyButton(text: "222")}{MyButton(text: "333")}{MyButton(text: "444")}
+            <br />
+            <br />
+
+            { names.Select(n => NoCButton(text: n)) }
+            
+            ...and then...
+            
+            { names.Where(n => n[0] != 't').Select(n => NoCButton(text: n)) }
+
+            <h2>Click on the buttons</h2>
+            <div>
+            outer div<br />
+                <div>
+                    middle div<br />
+                    <div>
+                        inner div<br />
+                        <button>allow propagation</button><br />
+                        <button id="stopPropagation">stop propagation</button><br />
+                        <button id="stopImmediatePropagation">immediate stop propagation</button>
+                    </div>
+                </div>
+            </div>
+            
+            {ComponentWithComponentInside()}
+
+            and one more for that big time key-rewind
+            {MyButton(text: "Nice to repeat you")}
+
         </body>
     </html>
     """);
 
 Html MyButton(string text, Action<Event>? onClick = null) => $"""
     <button onclick={onClick ?? OnClick}>
-        {text} Clicks: {c}
+        Hello {text}! Clicks: {c}
     </button>
     """;
 
-void OnClick(Event e)
+Html YourButton(string? text = null) => $"""
+    <button>
+        Clicks: {c} - {text ?? "whatevs"}
+    </button>
+    """;
+
+Html NoCButton(string text) => $"""
+    
+    <span>{text}, </span>
+    """;
+
+Html NoneButton() => $"""
+    <p>None</p>
+    """;
+
+Html ComponentWithComponentInside() => $"""
+    <div>
+        {MyButton(text: "3rd level?")}
+    </div>
+""";
+
+static void OnClick(Event.Mouse e)
 {
-    c++;
+    // c++;
+
+    // Console.WriteLine(e.X);
+    var x = e.X;
 }
 
-window.OnClick = e => Console.WriteLine($"win0: window.OnClick 0: {"e"}");
-window.AddEventListener("afterprint", (Event e) => Console.WriteLine($"win1: afterprint: {"e"}"));
-window.AddEventListener("dblclick", () => Console.WriteLine("win2: dblclick:"));
-window.AddEventListener("click", e => Console.WriteLine($"win3: click: {e.X} {e.Y}"));
-window.OnClick = e => Console.WriteLine($"win4: window.OnClick 1: {"e"}");
-
-window.Document.OnSelectionChange = e => Console.WriteLine($"doc5: {e.Type}");
-window.Document.AddEventListener("click", e => Console.WriteLine($"doc6: document.onclick: {e.X}"));
+// window.OnClick = e => Console.WriteLine($"win0: window.OnClick 0: {"e"}");
+// window.AddEventListener("afterprint", (Event e) => Console.WriteLine($"win1: afterprint: {"e"}"));
+// window.AddEventListener("dblclick", () => Console.WriteLine("win2: dblclick:"));
+// window.AddEventListener("click", e => d = e.X);
+// window.OnClick = e => Console.WriteLine($"win4: window.OnClick 1: {"e"}");
+// window.Document.OnSelectionChange = e => Console.WriteLine($"doc5: {e.Type}");
+// window.Document.AddEventListener("click", e => Console.WriteLine($"doc6: document.onclick: {e.X}"));
+window.AddEventListener("mousemove", e => c = (int)e.X);
 
 window.MapGet("/about", ctx => 
 {
