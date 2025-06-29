743f14bfde7a10afe123c98cc543b7228b95e748
Rylan Barnes <rylan@barn.es>
2025-06-28 20:00:24 -0500

    Keep for now.  Clean up later.

--- Benchmarks/Memory/Program.cs
+++ Benchmarks/Memory/Program.cs
@@ -9,13 +9,16 @@ BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
 
 [ShortRunJob]
 [MemoryDiagnoser]
+[OperationsPerSecond]
 public class Tests
 {
     readonly Pipe pipe = new();
     readonly NoOpComposer noOpComposer = new(null);
+    readonly NoOpWriter noOpWriter = new();
     readonly HtmlComposer htmlComposer = new(null);
     readonly XtmlComposer xtmlComposer = new(null, new(null, null));
     readonly SnapshotComposer snapshotComposer = new();
+    readonly FindKeyholeComposer findKeyholeComposer = new();
     readonly string name = "Rylan";
     readonly int c = 3;
     readonly State<int> cState = 3.AsState();
@@ -372,6 +375,328 @@ public class Tests
                 </body>
             </html>
             """);
+    }
+
+    record Point(double X, double Y);
+    private static List<Point> tiles;
+
+    [Benchmark]
+    public void SpiralToDevNull()
+    {
+        noOpWriter.Write(noOpComposer, $$"""
+            <!DOCTYPE html>
+            <html>
+                <head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <style>
+                body {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    background-color: #f0f0f0;
+                    margin: 0;
+                }
+                #wrapper {
+                    width: 960px;
+                    height: 720px;
+                    position: relative;
+                    background-color: white;
+                }
+                .tile {
+                    position: absolute;
+                    width: 10px;
+                    height: 10px;
+                    background-color: #333;
+                }
+                </style>
+                </head>
+                <body>
+                <div id="root">
+                    {{ tiles.Select(t => Tile(t.X, t.Y)) }}
+                </div>
+                </body>
+            </html>
+            """);
+    }
+    
+    [Benchmark]
+    public void SpiralToUtf8Buffer()
+    {
+        noOpWriter.Write(htmlComposer, $$"""
+            <!DOCTYPE html>
+            <html>
+                <head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <style>
+                body {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    background-color: #f0f0f0;
+                    margin: 0;
+                }
+                #wrapper {
+                    width: 960px;
+                    height: 720px;
+                    position: relative;
+                    background-color: white;
+                }
+                .tile {
+                    position: absolute;
+                    width: 10px;
+                    height: 10px;
+                    background-color: #333;
+                }
+                </style>
+                </head>
+                <body>
+                <div id="root">
+                    {{ tiles.Select(t => Tile(t.X, t.Y)) }}
+                </div>
+                </body>
+            </html>
+            """);
+    }
+
+    [Benchmark]
+    public void SpiralToUtf8Xtml()
+    {
+        noOpWriter.Write(xtmlComposer, $$"""
+            <!DOCTYPE html>
+            <html>
+                <head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <style>
+                body {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    background-color: #f0f0f0;
+                    margin: 0;
+                }
+                #wrapper {
+                    width: 960px;
+                    height: 720px;
+                    position: relative;
+                    background-color: white;
+                }
+                .tile {
+                    position: absolute;
+                    width: 10px;
+                    height: 10px;
+                    background-color: #333;
+                }
+                </style>
+                </head>
+                <body>
+                <div id="root">
+                    {{ tiles.Select(t => Tile(t.X, t.Y)) }}
+                </div>
+                </body>
+            </html>
+            """);
+    }
+
+    [Benchmark]
+    public void SpiralSearch()
+    {
+        Func<Html> html = () => $$"""
+            <!DOCTYPE html>
+            <html>
+                <head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <style>
+                body {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    background-color: #f0f0f0;
+                    margin: 0;
+                }
+                #wrapper {
+                    width: 960px;
+                    height: 720px;
+                    position: relative;
+                    background-color: white;
+                }
+                .tile {
+                    position: absolute;
+                    width: 10px;
+                    height: 10px;
+                    background-color: #333;
+                }
+                </style>
+                </head>
+                <body>
+                <div id="root">
+                    {{ tiles.Select(t => Tile(t.X, t.Y)) }}
+                </div>
+                </body>
+            </html>
+            """;
+        html.FindEventListener("");
+    }
+
+    static Html Tile(double x, double y) => $"""
+        <div 
+            class="tile"
+            style="left: {x:0.00}px; top: {y:0.00}px;">
+        </div>
+        """;
+
+
+
+
+
+    static readonly string web4Assets = ".";
+    static readonly string web4Head = "";
+    static List<EntryRecord> entries;
+    
+    [Benchmark]
+    public void GuidTableToDevNull()
+    {
+        noOpWriter.Write(noOpComposer, $"""
+            <!doctype html>
+            <html lang="en">
+                <head>
+                    <meta charset="utf-8" />
+                    <link rel="icon" href="{web4Assets}/favicon.png" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    {web4Head}
+                </head>
+                <body data-sveltekit-preload-data="hover">
+                    <div style="display: contents">{GuidTableBody()}</div>
+                </body>
+            </html>
+            """);
+    }
+    
+    [Benchmark]
+    public void GuidTableToUtf8Buffer()
+    {
+        noOpWriter.Write(htmlComposer, $"""
+            <!doctype html>
+            <html lang="en">
+                <head>
+                    <meta charset="utf-8" />
+                    <link rel="icon" href="{web4Assets}/favicon.png" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    {web4Head}
+                </head>
+                <body data-sveltekit-preload-data="hover">
+                    <div style="display: contents">{GuidTableBody()}</div>
+                </body>
+            </html>
+            """);
+    }
+
+    [Benchmark]
+    public void GuidTableToUtf8Xtml()
+    {
+        noOpWriter.Write(xtmlComposer, $"""
+            <!doctype html>
+            <html lang="en">
+                <head>
+                    <meta charset="utf-8" />
+                    <link rel="icon" href="{web4Assets}/favicon.png" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    {web4Head}
+                </head>
+                <body data-sveltekit-preload-data="hover">
+                    <div style="display: contents">{GuidTableBody()}</div>
+                </body>
+            </html>
+            """);
+    }
+
+    [Benchmark]
+    public void GuidTableSearch()
+    {
+        Func<Html> html = () => $"""
+            <!doctype html>
+            <html lang="en">
+                <head>
+                    <meta charset="utf-8" />
+                    <link rel="icon" href="{web4Assets}/favicon.png" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    {web4Head}
+                </head>
+                <body data-sveltekit-preload-data="hover">
+                    <div style="display: contents">{GuidTableBody()}</div>
+                </body>
+            </html>
+            """;
+        html.FindEventListener("");
+    }
+
+    static Html GuidTableBody() => $"""
+        <main>
+          <table>
+            {from entry in entries
+             select Entry(entry)}
+          </table>
+        </main>
+    """;
+
+    static Html Entry(EntryRecord entry) => $"""
+    <tr>
+      <td>{entry.ID}</td>
+      <td>{entry.Name}</td>
+    </tr>
+    """;
+
+    record class EntryRecord(string ID, string Name);
+
+
+
+    static Tests()
+    {
+        var wrapperWidth = 960;
+        var wrapperHeight = 720;
+        var cellSize = 10;
+        var centerX = wrapperWidth / 2d;
+        var centerY = wrapperHeight / 2d;
+
+        var angle = 0d;
+        var radius = 0d;
+
+        var tiles = new List<Point>();
+        var step = cellSize;
+
+        while (radius < Math.Min(wrapperWidth, wrapperHeight) / 2d)
+        {
+            var x = centerX + Math.Cos(angle) * radius;
+            var y = centerY + Math.Sin(angle) * radius;
+
+            if (x >= 0 && x <= wrapperWidth - cellSize && y >= 0 && y <= wrapperHeight - cellSize)
+            {
+                tiles.Add(new(x, y));
+            }
+
+            angle += 0.2;
+            radius += step * 0.015;
+        }
+
+        Tests.tiles = tiles;
+
+
+
+
+        Tests.entries = [.. Enumerable
+            .Range(0, 1000)
+            .Select(i => new EntryRecord(
+                ID: Guid.NewGuid().ToString(),
+                Name: Guid.NewGuid().ToString()
+            ))
+        ];
     }
 }
 
