ee8b181b1c3f8b3d06a48271cfa73d73d22e82f7
Rylan Barnes <rylan@barn.es>
2025-07-10 13:01:43 -0500

    This composer never got the latest refactoring

--- Benchmarks/Memory/NoOpComposer.cs
+++ Benchmarks/Memory/NoOpComposer.cs
@@ -15,18 +15,10 @@ public class NoOpComposer(IBufferWriter<byte> writer) : StreamingComposer(writer
     public override bool WriteMutableValue(ref Html parent, Uri value, string? format = null) => true;
     public override bool WriteMutableValue<T>(ref Html parent, T value, string? format = null) => true; // where T : struct, IUtf8SpanFormattable // (from base)
 
-    public override bool WriteMutableAttribute(ref Html parent, ReadOnlySpan<char> attrName, Func<Event, string> attrValue, string? expression = null) => true;
-    public override bool WriteMutableAttribute(ref Html parent, ReadOnlySpan<char> attrName, Func<Event, bool> attrValue, string? expression = null) => true;
-    public override bool WriteMutableAttribute<T>(ref Html parent, ReadOnlySpan<char> attrName, Func<Event, T> attrValue, string? format = null, string? expression = null) => true; // where T : struct, IUtf8SpanFormattable
-    public override bool WriteMutableAttribute(ref Html parent, ReadOnlySpan<char> attrName, Func<Event, Html> attrValue, string? expression = null) => true;
-    public override bool WriteMutableAttribute(ref Html parent, ReadOnlySpan<char> attrName, Func<Event, Color> attrValue, string? expression = null) => true;
-    public override bool WriteMutableAttribute(ref Html parent, ReadOnlySpan<char> attrName, Func<Event, Uri> attrValue, string? expression = null) => true;
-
     public override bool WriteEventListener(ref Html parent, Action listener, string? format = null, string? expression = null) => HandleNotSupported();
     public override bool WriteEventListener(ref Html parent, Action<Event> listener, string? format = null, string? expression = null) => HandleNotSupported();
     public override bool WriteEventListener(ref Html parent, Func<Task> listener, string? format = null, string? expression = null) => HandleNotSupported();
     public override bool WriteEventListener(ref Html parent, Func<Event, Task> listener, string? format = null, string? expression = null) => HandleNotSupported();
-    public override bool WriteEventListener(ref Html parent, ReadOnlySpan<char> argName, Action<object> listener, string? expression = null) => HandleNotSupported();
 
     private bool HandleNotSupported() => true;
     private bool HandleNotSupported(ReadOnlySpan<char> attributeName) => true;
