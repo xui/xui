8af989bea0c97595eb1bf436096b29635519357b
Rylan Barnes <rylan@barn.es>
2025-09-09 06:33:54 -0500

    Missed one

--- Web4/WebSockets/JsonRpcReader.cs
+++ Web4/WebSockets/JsonRpcReader.cs
@@ -3,48 +3,90 @@ using System.Text.Json;
 
 namespace Web4.WebSockets;
 
-struct JsonRpcReader
+ref struct JsonRpcReader(ReadOnlySequence<byte> sequence)
 {
-    public static JsonRpc Parse(ReadOnlySequence<byte> sequence)
-    {
-        using var perf = Debug.PerfCheck("JsonRpc.Parse"); // TODO: Remove PerfCheck
+    Utf8JsonReader reader = new(sequence);
 
+    public static JsonRpc ParseMessage(ReadOnlySequence<byte> sequence)
+    {
+        using var perf = Debug.PerfCheck("JsonRpcReader.Parse"); // TODO: Remove PerfCheck
+
+        var rpc = new JsonRpcReader(sequence);
         string? method = null;
         int? id = null;
-        try
-        {
-            var reader = new Utf8JsonReader(sequence);
 
-            while (reader.Read())
+        while (rpc.reader.Read())
+        {
+            if (rpc.reader.TokenType == JsonTokenType.PropertyName)
             {
-                if (reader.TokenType == JsonTokenType.PropertyName)
+                if (rpc.reader.ValueTextEquals("method"))
                 {
-                    if (reader.ValueTextEquals("method"))
-                    {
-                        reader.Read();
-                        ReadOnlySpan<byte> value = reader.HasValueSequence
-                            ? reader.ValueSequence.ToArray() // TODO: Allocates, but is rare?  Confirm.
-                            : reader.ValueSpan;
-                        method = Keymaker.GetKeyIfCached(value);
-                    }
-                    else if (reader.ValueTextEquals("params"))
-                    {
-                        reader.Skip();
-                    }
-                    else if (reader.ValueTextEquals("id"))
-                    {
-                        reader.Read();
-                        if (reader.TryGetInt32(out int i))
-                            id = i;
-                    }
+                    rpc.reader.Read();
+                    ReadOnlySpan<byte> value = rpc.reader.HasValueSequence
+                        ? rpc.reader.ValueSequence.ToArray() // TODO: Allocates, but is rare?  Confirm.
+                        : rpc.reader.ValueSpan;
+                    method = Keymaker.GetKeyIfCached(value);
+                }
+                else if (rpc.reader.ValueTextEquals("params"))
+                {
+                    rpc.reader.Skip();
+                }
+                else if (rpc.reader.ValueTextEquals("id"))
+                {
+                    rpc.reader.Read();
+                    if (rpc.reader.TryGetInt32(out int i))
+                        id = i;
                 }
             }
         }
-        catch (Exception ex)
+
+        return new(sequence) { Method = method, ID = id };
+    }
+
+    public static JsonRpcReader ParseParams(ReadOnlySequence<byte> sequence)
+    {
+        using var perf = Debug.PerfCheck("JsonRpcReader.ParseParams"); // TODO: Remove PerfCheck
+
+        var rpc = new JsonRpcReader(sequence);
+
+        while (rpc.reader.Read())
         {
-            Console.WriteLine(ex);
+            if (rpc.reader.TokenType == JsonTokenType.PropertyName)
+            {
+                if (rpc.reader.ValueTextEquals("params"))
+                {
+                    rpc.reader.Read();
+                    if (rpc.reader.TokenType == JsonTokenType.StartArray)
+                    {
+                        return rpc;
+                    }
+                }
+                else
+                {
+                    rpc.reader.Skip();
+                }
+            }
         }
 
-        return method is not null ? new(method, id) : JsonRpc.Error;
+        return rpc;
+    }
+
+    public int GetNextInt()
+    {
+        reader.Read();
+        return reader.GetInt32();
+    }
+
+    public string GetNextString()
+    {
+        reader.Read();
+        return reader.GetString()!;
+    }
+
+    public LazyEvent GetNextEvent()
+    {
+        reader.Read();
+        reader.Skip();
+        return new(sequence);
     }
 }
