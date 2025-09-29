1749fff69bcbf2ff6e2c6d73479b09c80157d1a6
Rylan Barnes <rylan@barn.es>
2025-09-29 04:39:09 -0500

    Remove the unecessary

--- Web4/IKeyholes.cs
+++ Web4/IKeyholes.cs
@@ -3,15 +3,15 @@ namespace Web4;
 // TODO: It is awkward that this interface is here and not in the core project, thanks to Keyholes.
 public interface IKeyholes
 {
-    public void SetTextNode(ref Keyhole oldKeyhole, ref Keyhole newKeyhole);
+    void SetTextNode(ref Keyhole oldKeyhole, ref Keyhole newKeyhole);
 
-    public void SetAttribute(ref Keyhole oldKeyhole, ref Keyhole newKeyhole);
+    void SetAttribute(ref Keyhole oldKeyhole, ref Keyhole newKeyhole);
 
-    public void SetAttribute(string key, Span<Keyhole> oldKeyholes, Span<Keyhole> newKeyholes);
+    void SetAttribute(string key, Span<Keyhole> oldKeyholes, Span<Keyhole> newKeyholes);
 
-    public void SetElement(string key, Span<Keyhole> oldKeyholes, Span<Keyhole> newKeyholes, string? transition = null);
+    void SetElement(string key, Span<Keyhole> oldKeyholes, Span<Keyhole> newKeyholes, string? transition = null);
 
-    public void AddElement(string priorKey, string key, Span<Keyhole> keyholes, string? transition = null);
+    void AddElement(string priorKey, string key, Span<Keyhole> keyholes, string? transition = null);
 
-    public void RemoveElement(string key, string? transition = null);
+    void RemoveElement(string key, string? transition = null);
 }
