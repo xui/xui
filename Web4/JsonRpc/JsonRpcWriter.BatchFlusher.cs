0fbf7eda6fe504025d4e0eb0824d80337ba51552
Rylan Barnes <rylan@barn.es>
2025-10-05 06:55:02 -0500

    Minimized memory allocations in Post

--- Web4/JsonRpc/JsonRpcWriter.BatchFlusher.cs
+++ Web4/JsonRpc/JsonRpcWriter.BatchFlusher.cs
@@ -7,22 +7,28 @@ partial class JsonRpcWriter
 {
     private class BatchFlusher : SynchronizationContext
     {
+        private record struct StateHolder(SendOrPostCallback Callback, object? State, BatchFlusher BatchFlusher);
         public ChannelWriter<ReadOnlySequence<byte>>? Flusher { get; set; }
 
-        public override void Post(SendOrPostCallback d, object? state)
+        public override void Post(SendOrPostCallback callback, object? state)
         {
-            // TODO: Another allocation (transport.Flush()).  Can I nuke it using state?
-            base.Post(s =>
-            {
-                // Set it again because AspNetWebSocket uses ConfigureAwait(false)
-                SetSynchronizationContext(this);
+            using var perf = Debug.PerfCheck("BatchFlusher.Post"); // TODO: Remove PerfCheck
 
-                d(s);
+            ThreadPool.QueueUserWorkItem<StateHolder>(
+                callBack: static s =>
+                {
+                    // Set it again because AspNetWebSocket uses ConfigureAwait(false)
+                    SetSynchronizationContext(s.BatchFlusher);
 
-                var flusher = Flusher ?? throw new NullReferenceException("🛑 Cannot use BatchFlusher without a Flusher or expect the system to get into a bad state.  Needs investigating.");
-                var writer = JsonRpcWriter.Current(flusher);
-                writer.Flush();
-            }, state);
+                    s.Callback(s.State);
+
+                    var flusher = s.BatchFlusher.Flusher ?? throw new NullReferenceException("🛑 Cannot use BatchFlusher without a Flusher or expect the system to get into a bad state.  Needs investigating.");
+                    var writer = JsonRpcWriter.Current(flusher);
+                    writer.Flush();
+                },
+                state: new(callback, state, this),
+                preferLocal: false
+            );
         }
     }
 }
