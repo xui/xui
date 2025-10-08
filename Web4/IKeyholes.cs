06d0723588e432a6f45bf3b3669e909c1425b7b9
Rylan Barnes <rylan@barn.es>
2025-10-08 05:31:50 -0500

    Cleanup

--- Web4/IKeyholes.cs
+++ Web4/IKeyholes.cs
@@ -4,15 +4,10 @@ namespace Web4;
 public interface IKeyholes
 {
     void SetTextNode(ref Keyhole oldKeyhole, ref Keyhole newKeyhole);
-
     void SetAttribute(ref Keyhole oldKeyhole, ref Keyhole newKeyhole);
-
     void SetAttribute(string key, Span<Keyhole> oldKeyholes, Span<Keyhole> newKeyholes);
-
     void SetElement(string key, Span<Keyhole> oldKeyholes, Span<Keyhole> newKeyholes, string? transition = null);
-
     void AddElement(string priorKey, string key, Span<Keyhole> keyholes, string? transition = null);
-
     void RemoveElement(string key, string? transition = null);
     void DispatchEvent(Action listener);
     void DispatchEvent<T>(Action<Event> listener, T @event) where T : struct, Event;
