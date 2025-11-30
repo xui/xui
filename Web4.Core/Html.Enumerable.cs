cad360ee3c0c1116b9139434d4c9193348efe14b
Rylan Barnes <rylan@barn.es>
2025-11-29 18:03:23 -0600

    I think I'll keep this.  Roughly 10% faster?

--- Web4.Core/Html.Enumerable.cs
+++ Web4.Core/Html.Enumerable.cs
@@ -2,47 +2,44 @@ namespace Web4;
 
 public static class HtmlExtensions
 {
-    public static Html.Enumerable<T> Select<T>(this IEnumerable<T> source, Func<T, Html> selector)
-        => new(source, selector);
+    public static Html.IList<T> Select<T>(
+        this IList<T> source, 
+        Func<T, Html> selector)
+            => new(source, selector);
+
+    // public static Html.IEnumerable<T> Select<T>(
+    //     this IEnumerable<T> source, 
+    //     Func<T, Html> selector)
+    //         => new(source, selector);
 }
 
 public ref partial struct Html
 {
-    public struct Enumerable<T>(IEnumerable<T> source, Func<T, Html> selector)
+    public struct IList<T>(System.Collections.Generic.IList<T> source, Func<T, Html> selector)
     {
-        public readonly int Count => source.Count();
-        public readonly Enumerator<T> GetEnumerator() => new(source, selector);
-    }
+        public readonly int Count => source.Count;
+        public readonly Enumerator GetEnumerator() => new(source, selector);
 
-    public struct Enumerator<T>(IEnumerable<T> source, Func<T, Html> selector)
-    {
-        int index = -1;
-        readonly IEnumerator<T>? enumerator = source is not IList<T> ? source.GetEnumerator() : null;
-
-        public readonly Html Current => source switch
+        public struct Enumerator(System.Collections.Generic.IList<T> list, Func<T, Html> selector)
         {
-            IList<T> list => selector(list[index]),
-            _ => selector(enumerator!.Current),
-        };
-
-        public readonly (Func<T, Html> selector, T) CurrentDeconstructed
-        {
-            get
-            {
-                T item = source switch
-                {
-                    IList<T> list => list[index],
-                    _ => enumerator!.Current,
-                };
-
-                return (selector, item);
-            }
+            int index = -1;
+            public readonly Html Current => selector(list[index]);
+            public readonly (Func<T, Html> selector, T) CurrentDeconstructed => (selector, list[index]);
+            public bool MoveNext() => ++index < list.Count;
         }
-
-        public bool MoveNext() => source switch
-        {
-            IList<T> list => ++index < list.Count,
-            _ => enumerator!.MoveNext(),
-        };
     }
+
+    // public struct IEnumerable<T>(System.Collections.Generic.IEnumerable<T> source, Func<T, Html> selector)
+    // {
+    //     public readonly int Count => source.Count();
+    //     public readonly Enumerator GetEnumerator() => new(source, selector);
+
+    //     public struct Enumerator(System.Collections.Generic.IEnumerable<T> enumerable, Func<T, Html> selector)
+    //     {
+    //         readonly IEnumerator<T> enumerator = enumerable.GetEnumerator();
+    //         public readonly Html Current => selector(enumerator.Current);
+    //         public readonly (Func<T, Html> selector, T) CurrentDeconstructed => (selector, enumerator.Current);
+    //         public bool MoveNext() => enumerator.MoveNext();
+    //     }
+    // }
 }
