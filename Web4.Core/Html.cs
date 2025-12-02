9588f7576200258b1dabf7505d6647d15d7717b2
Rylan Barnes <rylan@barn.es>
2025-12-02 06:26:15 -0600

    Drop this guy too

--- Web4.Core/Html.cs
+++ Web4.Core/Html.cs
@@ -204,17 +204,6 @@ public ref partial struct Html
         return @continue;
     }
 
-    // // EX: <div>{ new MyComponent(name: "Rylan") }</div>
-    // public bool AppendFormatted<TRender>(TRender renderer, string? format = null, [CallerArgumentExpression(nameof(renderer))] string? expression = null) where TRender : struct, IRender
-    // {
-    //     if (IsEven(Cursor))
-    //         AppendLiteral(string.Empty);
-
-    //     var @continue = composer.WriteMutableElement(ref this, ref renderer, format, expression);
-    //     Cursor++;
-    //     return @continue;
-    // }
-
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static bool IsEven(int number) => number % 2 == 0;
 }
