9d813b424225a395ead96724da356170ddd349e1
Rylan Barnes <rylan@barn.es>
2025-12-02 05:31:19 -0600

    Now put them back

--- Benchmarks/Memory/NoOpComposer.cs
+++ Benchmarks/Memory/NoOpComposer.cs
@@ -19,4 +19,15 @@ public class NoOpComposer(IBufferWriter<byte> writer) : StreamingComposer(writer
     public override bool WriteEventListener(ref Html parent, Action<Event> listener, string? format = null, string? expression = null) => true;
     public override bool WriteEventListener(ref Html parent, Func<Task> listener, string? format = null, string? expression = null) => true;
     public override bool WriteEventListener(ref Html parent, Func<Event, Task> listener, string? format = null, string? expression = null) => true;
+
+    public override bool WriteMutableNode<T>(ref Html parent, Html.Enumerable<T> partials, string? format = null, string? expression = null)
+    {
+        foreach (var partial in partials) { }
+        return true;
+    }
+
+
+    public override void OnHtmlPartialBegins(ref Html parent) { }
+    public override bool OnHtmlPartialEnds(ref Html parent, scoped Html partial, string? format = null, string? expression = null) => true;
+
 }
