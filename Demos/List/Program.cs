e0959c611711cc497ae26ab1f9fd31bfe004098a
Rylan Barnes <rylan@barn.es>
2025-12-18 08:16:02 -0600

    Repurpose this for other scratch

--- Demos/List/Program.cs
+++ Demos/List/Program.cs
@@ -3,85 +3,17 @@ global using Web4;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var name = "World";
-List<string> names = ["one", "two", "three", "four", "five", "six", "seven"];
+var _name = "World";
 
-app.MapWeb4("/", () => $"""
-    <list>
-        {names.Select(str => $"""
-            <lr>
-                <ld>{ Icons.Globe }</ld>
-                <ld>Hello { name }, { str }!</ld>
-            </lr>
-        """):zoom-fade}
-        <button onclick={ EditRandom }>Edit Random</button>
-        <button onclick={ AddOne }>Add One</button>
-        <button onclick={ RemoveOne }>Remove One</button>
-        <button onclick={ Move1 }>Move 1</button>
-        <button onclick={ Swap2 }>Swap 2</button>
-        <button onclick={ Shuffle }>Shuffle</button>
-        <button onclick={ DoThingAsync }>Async</button>
-    </list>
-""");
+app.MapGet("/classic", () => "Hello World");
+app.MapGet("/wat1/{name}", (string name) => "Hello {name}");
+app.MapGet("/hello/{name:alpha}", (string name) => $"Hello {name}!");
 
-void EditRandom(Event e)
-{
-    var index = Random.Shared.Next(names.Count);
-    names[index] += " 😀";
-}
+app.MapGet("/wat2/{c}", (int c) => $"Hello {c:c}");
+app.MapGet("/interpolation", () => $"Hello {14:c}");
 
-void AddOne()
-{
-    names.Add(names.Count.ToString());
-}
+app.MapWeb4("web4", () => $"Hello {14:c}");
+// app.MapWeb4("web4/ctx", ctx => $"Hello {14:c}");
 
-void RemoveOne()
-{
-    names.RemoveAt(names.Count - 1);
-}
-
-void Move1()
-{
-    var i1 = Random.Shared.Next(names.Count);
-    var i2 = Random.Shared.Next(names.Count);
-    var item = names[i1];
-    names.RemoveAt(i1);
-    names.Insert(i2, item);
-}
-
-void Swap2()
-{
-    var i1 = Random.Shared.Next(names.Count);
-    var i2 = Random.Shared.Next(names.Count);
-    (names[i1], names[i2]) = (names[i2], names[i1]);
-}
-
-void Shuffle()
-{
-    names = names.OrderBy(n => Random.Shared.Next()).ToList();
-}
-
-async Task DoThingAsync(Event.Subsets.View e)
-{
-    var window = e.View;
-    var console = window.Console;
-
-    console.Log("one");
-    console.Log("two");
-    await Task.Delay(2000);
-    
-    console.Log("three");
-    console.Log("four");
-    await Task.Delay(2000);
-    
-    console.Log("five");
-    console.Log("six");
-    int i = 1;
-    i--;
-    i /= i;
-    
-    window.Alert("Neato");
-    console.Log("seven");
-}
 
 app.Run();
