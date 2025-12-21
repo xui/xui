efb4f7cd0a330c9d709003b9dc3bc6e61096f698
Rylan Barnes <rylan@barn.es>
2025-12-21 08:10:38 -0600

    There is a difference but it's not here

--- Web4/Composers/SnapshotComposer.cs
+++ Web4/Composers/SnapshotComposer.cs
@@ -196,7 +196,6 @@ public class SnapshotComposer : ResultComposer<Keyhole[]>
             keyhole.IsValueAnAttribute = isWritingAttribute;
         }
 
-        // TODO: Optimize.  The classes are faster (no switch?).  Test with dotnet run -c Release --filter '*SnapshotComposer*
         switch (value)
         {
             case int i:
