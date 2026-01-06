8047ffc874bd8e470107fb8a337204394b83726f
Rylan Barnes <rylan@barn.es>
2026-01-06 08:48:56 -0600

    DRY

--- Web4/RawText.cs
+++ Web4/RawText.cs
@@ -10,14 +10,9 @@ namespace Web4;
 [StructLayout(LayoutKind.Auto)]
 public readonly ref struct RawText(int literalLength, int formattedCount, IBufferWriter<byte> writer)
 {
-    public readonly void AppendLiteral(ReadOnlySpan<char> value)
-    {
-        Span<byte> buffer = writer.GetSpan(value.Length);
-        int length = Encoding.UTF8.GetBytes(value, buffer);
-        writer.Advance(length);
-    }
-
-    public readonly void AppendFormatted(ReadOnlySpan<char> value)
+    public readonly void AppendLiteral(ReadOnlySpan<char> value) => Append(value);
+    public readonly void AppendFormatted(ReadOnlySpan<char> value) => Append(value);
+    private readonly void Append(ReadOnlySpan<char> value)
     {
         Span<byte> buffer = writer.GetSpan(value.Length);
         int length = Encoding.UTF8.GetBytes(value, buffer);
