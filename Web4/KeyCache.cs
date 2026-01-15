7b1a17e133fe4c572459334ff52fcaabfbd45ef7
Rylan Barnes <rylan@barn.es>
2026-01-15 05:27:13 -0600

    Keep

---
+++ Web4/KeyCache.cs
@@ -0,0 +1,37 @@
+namespace Web4;
+
+class KeyCache
+{
+    private readonly List<KeyCache?> children = [];
+    public static KeyCache Root { get; } = new KeyCache(null!, "key"u8.ToArray());
+    public byte[] Key { get; private set; }
+    public KeyCache Parent { get; private set; }
+
+    public byte[]? this[int index]
+    {
+        get => index < children.Count ? children[index]?.Key : null;
+        set
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            while (children.Count < index)
+                children.Add(null);
+
+            if (index == children.Count)
+                children.Add(new KeyCache(this, value));
+            else
+                children[index] = new KeyCache(this, value);
+        }
+    }
+
+    private KeyCache(KeyCache parent, byte[] key)
+    {
+        Parent = parent;
+        Key = key;
+    }
+
+    public KeyCache NextGeneration(int index)
+    {
+        return children[index] ??= new KeyCache(this, Parent.Key);
+    }
+}
