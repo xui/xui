164408985c1a6bcd4f0793c2ec65679042314350
Rylan Barnes <rylan@barn.es>
2026-01-21 06:35:46 -0600

    Same stuff, simpler syntax

--- Web4/Composers/FindKeyholeComposer.cs
+++ Web4/Composers/FindKeyholeComposer.cs
@@ -11,7 +11,7 @@ public class FindKeyholeComposer : KeyholeComposer
     private EventListener eventListener = default;
     private byte[] keyBuffer = [];
     private Memory<byte> searchKey;
-    private bool shortCircuitTailCalls = false;
+    private bool isFound = false;
 
     public EventListener FindEventListener(ReadOnlySpan<byte> key, Func<Html> template)
     {
@@ -41,37 +41,35 @@ public class FindKeyholeComposer : KeyholeComposer
         // Do something interesting with the result.
         return result;
     }
-    
-    public override bool OnHtmlEnd(ref Html parent, scoped Html html, string? format = null, string? expression = null)
-    {
-        if (shortCircuitTailCalls)
-            return false;
-        return base.OnHtmlEnd(ref parent, html, format, expression);;
-    }
 
-    public override bool OnStringKeyhole(ref Html parent, string value) => MoveNextKeyAndComplete();
-    public override bool OnBoolKeyhole(ref Html parent, bool value) => MoveNextKeyAndComplete();
-    public override bool OnIntKeyhole(ref Html parent, int value, string? format = null) => MoveNextKeyAndComplete();
-    public override bool OnLongKeyhole(ref Html parent, long value, string? format = null) => MoveNextKeyAndComplete();
-    public override bool OnFloatKeyhole(ref Html parent, float value, string? format = null) => MoveNextKeyAndComplete();
-    public override bool OnDoubleKeyhole(ref Html parent, double value, string? format = null) => MoveNextKeyAndComplete();
-    public override bool OnDecimalKeyhole(ref Html parent, decimal value, string? format = null) => MoveNextKeyAndComplete();
-    public override bool OnDateTimeKeyhole(ref Html parent, DateTime value, string? format = null) => MoveNextKeyAndComplete();
-    public override bool OnDateOnlyKeyhole(ref Html parent, DateOnly value, string? format = null) => MoveNextKeyAndComplete();
-    public override bool OnTimeSpanKeyhole(ref Html parent, TimeSpan value, string? format = null) => MoveNextKeyAndComplete();
-    public override bool OnTimeOnlyKeyhole(ref Html parent, TimeOnly value, string? format = null) => MoveNextKeyAndComplete();
-    public override bool OnColorKeyhole(ref Html parent, Color value, string? format = null) => MoveNextKeyAndComplete();
-    public override bool OnUriKeyhole(ref Html parent, Uri value, string? format = null) => MoveNextKeyAndComplete();
-    private bool MoveNextKeyAndComplete()
-    {
-        if (shortCircuitTailCalls)
-            return false;
-        return base.OnKeyhole();
-    }
+    // Note: Returning false shortcircuits InterpolatedStringHandler from calling any
+    // subsequent AppendFormatted() or AppendLiteral() methods.  
+    // And isFound is used to trickle that upwards to all parent Htmls.
+    
+    protected override bool OnKeyhole()
+        => !isFound && base.OnKeyhole();
+
+    public override bool OnHtmlBegin(ref Html html)
+        => !isFound && base.OnHtmlBegin(ref html);
+
+    public override bool OnHtmlKeyhole(ref Html parent, scoped Html html, string? format = null, string? expression = null)
+        => !isFound && base.OnHtmlKeyhole(ref parent, html, format, expression);
+
+    public override bool OnHtmlEnd(ref Html parent, scoped Html html, string? format = null, string? expression = null)
+        => !isFound && base.OnHtmlEnd(ref parent, html, format, expression);
+
+    public override bool OnIteratorBegin(ref Html parent, ref Html htmls, string? format = null, string? expression = null)
+        => !isFound && base.OnIteratorBegin(ref parent, ref htmls, format, expression);
+
+    public override bool OnIteratorKeyhole<T>(ref Html parent, ref Html htmls, Html.Enumerable<T> enumerable, string? format = null, string? expression = null)
+        => !isFound && base.OnIteratorKeyhole(ref parent, ref htmls, enumerable, format, expression);
+
+    public override bool OnIteratorEnd(ref Html parent, ref Html htmls, string? format = null, string? expression = null)
+        => !isFound && base.OnIteratorEnd(ref parent, ref htmls, format, expression);
 
     public override bool OnListener(ref Html parent, Action listener, string? format = null, string? expression = null)
     {
-        if (shortCircuitTailCalls)
+        if (isFound)
             return false;
         
         base.OnListener(ref parent, listener, format, expression);
@@ -79,16 +77,16 @@ public class FindKeyholeComposer : KeyholeComposer
         if (Key.SequenceEqual(searchKey.Span))
         {
             eventListener.Action = listener;
-            shortCircuitTailCalls = true;
-            // Found it so save some time: return false to short circuit any following calls to this html.Append*().
+            isFound = true;
             return false;
         }
+
         return true;
     }
 
     public override bool OnListener(ref Html parent, Action<Event> listener, string? format = null, string? expression = null)
     {
-        if (shortCircuitTailCalls)
+        if (isFound)
             return false;
             
         base.OnListener(ref parent, listener, format, expression);
@@ -96,16 +94,16 @@ public class FindKeyholeComposer : KeyholeComposer
         if (Key.SequenceEqual(searchKey.Span))
         {
             eventListener.ActionEvent = listener;
-            shortCircuitTailCalls = true;
-            // Found it so save some time: return false to short circuit any following calls to this html.Append*().
+            isFound = true;
             return false;
         }
+
         return true;
     }
 
     public override bool OnListener(ref Html parent, Func<Task> listener, string? format = null, string? expression = null)
     {
-        if (shortCircuitTailCalls)
+        if (isFound)
             return false;
             
         base.OnListener(ref parent, listener, format, expression);
@@ -113,16 +111,16 @@ public class FindKeyholeComposer : KeyholeComposer
         if (Key.SequenceEqual(searchKey.Span))
         {
             eventListener.Func = listener;
-            shortCircuitTailCalls = true;
-            // Found it so save some time: return false to short circuit any following calls to this html.Append*().
+            isFound = true;
             return false;
         }
+
         return true;
     }
 
     public override bool OnListener(ref Html parent, Func<Event, Task> listener, string? format = null, string? expression = null)
     {
-        if (shortCircuitTailCalls)
+        if (isFound)
             return false;
             
         base.OnListener(ref parent, listener, format, expression);
@@ -130,44 +128,17 @@ public class FindKeyholeComposer : KeyholeComposer
         if (Key.SequenceEqual(searchKey.Span))
         {
             eventListener.FuncEvent = listener;
-            shortCircuitTailCalls = true;
-            // Found it so save some time: return false to short circuit any following calls to this html.Append*().
+            isFound = true;
             return false;
         }
-        return true;
-    }
-
-    public override bool OnIteratorBegin(ref Html parent, ref Html htmls, string? format = null, string? expression = null)
-    {
-        if (shortCircuitTailCalls)
-            return false;
         
-        return base.OnIteratorBegin(ref parent, ref htmls, format, expression);
-    }
-
-    public override bool OnIteratorKeyhole<T>(ref Html parent, ref Html htmls, Html.Enumerable<T> enumerable, string? format = null, string? expression = null)
-    {
-        if (shortCircuitTailCalls)
-            return false;
-        
-        foreach (var html in enumerable)
-            htmls.AppendFormatted(html);
-
         return true;
-    }
-
-    public override bool OnIteratorEnd(ref Html parent, ref Html htmls, string? format = null, string? expression = null)
-    {
-        if (shortCircuitTailCalls)
-            return false;
-
-        return base.OnIteratorEnd(ref parent, ref htmls, format, expression);
     }
 
     public override void Reset()
     {
         searchKey = default;
-        shortCircuitTailCalls = false;
+        isFound = false;
         eventListener = default;
         base.Reset();
     }
