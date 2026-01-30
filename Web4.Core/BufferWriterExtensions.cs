67bd09213e80ee75484697a593775eabf97332a6
Rylan Barnes <rylan@barn.es>
2026-01-30 04:51:25 -0600

    As you wish

--- Web4.Core/BufferWriterExtensions.cs
+++ Web4.Core/BufferWriterExtensions.cs
@@ -68,18 +68,6 @@ public static partial class BufferWriterExtensions
         bufferWriter.Write(text3);
     }
 
-    // TODO: Remove this one after KeyMaker switches to utf8.
-    public static void Write(
-        this IBufferWriter<byte> bufferWriter, 
-        ReadOnlySpan<byte> text1,
-        string text2,
-        ReadOnlySpan<byte> text3)
-    {
-        bufferWriter.Write(text1);
-        bufferWriter.Write(text2);
-        bufferWriter.Write(text3);
-    }
-
     public static void Write(
         this IBufferWriter<byte> writer, // This one defaults to HtmlComposer (see Html constructor below)
         [InterpolatedStringHandlerArgument("writer")] ref Html html)
