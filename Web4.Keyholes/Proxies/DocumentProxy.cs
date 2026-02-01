e64600da1048044deb97d92a4b57295f0f865cb8
Rylan Barnes <rylan@barn.es>
2026-01-31 22:20:00 -0600

    Trashed long ago

--- Web4.Keyholes/Proxies/DocumentProxy.cs
+++
@@ -1,9 +0,0 @@
-// namespace Web4.Proxies;
-
-// public struct DocumentProxy(IWeb4Transport transport)
-// {
-//     public string? Title { get; set; }
-    
-//     public readonly WindowProxy DefaultView { get => new(transport); }
-// }
-
